f7b8ca1ee5e078967fe97768d2b60998e993e0a5
dschykerynec <dschykerynec@gmail.com>
2025-02-07 17:20:19 -0800

    removed test code that forced telemetry window to open

--- src/main/index.ts
+++ src/main/index.ts
@@ -257,9 +257,7 @@ app.whenReady().then(() => {
     electronApp.setAppUserModelId('com.electron')
 
     setupWebSocketUtility()
-
-    setUpOverlays()
-
+    
     // Default open or close DevTools by F12 in development
     // and ignore CommandOrControl + R in production.
     // see https://github.com/alex8088/electron-toolkit/tree/master/packages/utils
