e5cd9e446c1ce5f8117556d19ad358959a6c935d
dschykerynec <dschykerynec@gmail.com>
2025-03-08 01:56:36 -0800

    set draft mode to true
    release description is automatically set to the content of the last commit which is often something useless like "incremented version". this will require manualy going on github to update and publish the release, but ensures much more useful release notes, since most releases will encompass more changes than just a single commit.

--- forge.config.cjs
+++ forge.config.cjs
@@ -1,43 +1,43 @@
-require('dotenv').config({ path: '.env' });
+require('dotenv').config({ path: '.env' })
 
 module.exports = {
-    packagerConfig: {
-      ignore: [
-        /^\/src/,
-        /(.eslintrc.json)|(.gitignore)|(electron.vite.config.ts)|(forge.config.cjs)|(tsconfig.*)/,
-      ],
-    },
-    rebuildConfig: {},
-    makers: [
-      {
-        name: '@electron-forge/maker-squirrel',
-        config: {},
-      },
-      {
-        name: '@electron-forge/maker-zip',
-        platforms: ['darwin'],
-      },
-      {
-        name: '@electron-forge/maker-deb',
-        config: {},
-      },
-      {
-        name: '@electron-forge/maker-rpm',
-        config: {},
-      },
-    ],
-    publishers: [
-      {
-        name: '@electron-forge/publisher-github',
-        config: {
-          repository: {
-            owner: process.env.REPOSITORY_OWNER,
-            name: process.env.REPOSITORY_NAME,
-            authToken: process.env.GITHUB_TOKEN,
-          },
-          prerelease: false,
-          draft: false
-        }
-      }
+  packagerConfig: {
+    ignore: [
+      /^\/src/,
+      /(.eslintrc.json)|(.gitignore)|(electron.vite.config.ts)|(forge.config.cjs)|(tsconfig.*)/
     ]
-  };
+  },
+  rebuildConfig: {},
+  makers: [
+    {
+      name: '@electron-forge/maker-squirrel',
+      config: {}
+    },
+    {
+      name: '@electron-forge/maker-zip',
+      platforms: ['darwin']
+    },
+    {
+      name: '@electron-forge/maker-deb',
+      config: {}
+    },
+    {
+      name: '@electron-forge/maker-rpm',
+      config: {}
+    }
+  ],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: process.env.REPOSITORY_OWNER,
+          name: process.env.REPOSITORY_NAME,
+          authToken: process.env.GITHUB_TOKEN
+        },
+        prerelease: false,
+        draft: true
+      }
+    }
+  ]
+}
