d7842971014bc195db3a2810f8cdfc4dd798e8be
dschykerynec <dschykerynec@gmail.com>
2025-03-18 23:42:52 -0700

    added toggleDraggableWindowsKeybind

--- src/types/userPreferences.ts
+++ src/types/userPreferences.ts
@@ -1,9 +1,11 @@
 export class UserPreferences {
   mainMenuPosition: number[]
   telemetryOverlayPosition: number[]
+  toggleDraggableWindowsKeybind: string
 
   constructor() {
     this.mainMenuPosition = [0, 0]
     this.telemetryOverlayPosition = [0, 0]
+    this.toggleDraggableWindowsKeybind = 'Control+Alt+Shift+M'
   }
 }
